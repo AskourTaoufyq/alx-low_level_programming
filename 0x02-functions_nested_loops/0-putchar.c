c4aa160e40778ef8d036f72047d2adf56aaeba17
AskourTaoufyq <136017759+AskourTaoufyq@users.noreply.github.com>
2023-07-18 17:46:40 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - print _putchar
  * is written by mbah
@@ -17,4 +17,23 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
+}
+
+
+1-alphabet.c
+
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
