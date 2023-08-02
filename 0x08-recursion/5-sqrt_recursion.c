1676cd6058cd0abcfd719f2cfbbfc3db2678d74c
AskourTaoufyq <kosa477kacm@gmail.com>
2023-08-02 08:14:41 -0700

    C recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,6 +8,13 @@ int actual_sqrt_recursion(int n, int i);
  *
  * Return: the resulting square root
  */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
@@ -16,13 +23,6 @@ int actual_sqrt_recursion(int n, int i);
  *
  * Return: the resulting square root
  */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	return (actual_sqrt_recursion(n, 0));
-}
-
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
