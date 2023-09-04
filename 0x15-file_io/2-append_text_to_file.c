2a3b3547b843496a87c932c8b2acd4072ef8acfd
AskourTaoufyq <kosa477kacm@gmail.com>
2023-09-04 08:56:05 -0700

    c file io

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: pointer to the name of the file
+ * @text_content: is the NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure
+ **/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, l;
+
+	if (filename == NULL)
+		return (-1);
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	if (text_content != NULL)
+	{
+		l = strlen(text_content);
+		w = write(o, text_content, l);
+	}
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 #define READ_BUF_SIZE 1024
 
 int _putchar(char c);
